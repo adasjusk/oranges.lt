ac0061dd74b7eec80e743f897b72a8626d46c5bf
Adas <adasjusk@oranges.lt>
2025-07-04 16:19:23 +0300

    Update get.ps1

--- get.ps1
+++ get.ps1
@@ -39,7 +39,7 @@ $process.WaitForExit()
 
 # Report the result
 if ($process.ExitCode -eq 0) {
-    Write-Host "Program executed successfully."
+    Write-Host "Have a good day, bye 👋."
 } else {
     Write-Host "Program exited with code $($process.ExitCode)."
 }
